356274333cbb4485f53ee9511e018134f9f9e581
José Miguel <clasiccode@gmail.com>
2021-09-20 15:19:27 +0200

    Update version

--- _components/index.ts
+++
@@ -1,3 +0,0 @@
-import { RouterViewComponent } from "./router-view/router-view.component"
-
-export const routerView = RouterViewComponent
--- directive/index.ts
+++
@@ -1,5 +0,0 @@
-import { RouterLinkDirective } from "./router-link.directive";
-
-export const RouterExtendDirectives = [
-    RouterLinkDirective
-]
