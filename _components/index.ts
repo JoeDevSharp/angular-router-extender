e0eb85b160f5e1580691e6913c614b44430f3866
José Miguel <clasiccode@gmail.com>
2021-09-20 10:52:31 +0200

    update version

--- _components/index.ts
+++ _components/index.ts
@@ -1,5 +1,3 @@
 import { RouterViewComponent } from "./router-view/router-view.component"
 
-export const routerExtendComponents = [
-    RouterViewComponent
-]
+export const routerView = RouterViewComponent
